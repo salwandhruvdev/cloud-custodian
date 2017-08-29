3673faaa4a16a1b9075db4e6871349c96979c01a
dhruv salwan <dhruv.salwan@capitalone.com>
2017-08-29 15:05:54 -0400

    Add generate_arn to fix EFS tagging

--- c7n/resources/efs.py
+++ c7n/resources/efs.py
@@ -12,12 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from __future__ import absolute_import, division, print_function, unicode_literals
+import functools
 
 from c7n.actions import Action
 from c7n.manager import resources
 from c7n.query import QueryResourceManager
 from c7n.tags import universal_augment, register_universal_tags
-from c7n.utils import local_session, type_schema, get_retry
+from c7n.utils import local_session, type_schema, get_retry, generate_arn
 
 
 @resources.register('efs')
@@ -40,6 +41,18 @@ class ElasticFileSystem(QueryResourceManager):
     def augment(self, resources):
         return universal_augment(
             self, super(ElasticFileSystem, self).augment(resources))
+
+    @property
+    def generate_arn(self):
+        if self._generate_arn is None:
+            self._generate_arn = functools.partial(
+                generate_arn,
+                'elasticfilesystem',
+                region=self.config.region,
+                account_id=self.account_id,
+                resource_type='file-system',
+                separator='/')
+        return self._generate_arn
 
 
 register_universal_tags(
